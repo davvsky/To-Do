dc2fae9a302f1c737dc4b33de5c349e6711c486c
Boruta <38374158+TheZerter@users.noreply.github.com>
2025-10-30 22:41:41 +0100

    Add full to-do functionality with reusable functions
    - Add input validations to prevent empty task
    - Create reusable functions for all buttons (Done, Abandon, Delete, Reactivate)
    - Add ability to move task between Active, Done and Abandon lists
    - Add Reactivate button to restor abandoned tasks to Active

--- script.js
+++ script.js
@@ -1,13 +1,90 @@
 let taskInput = document.getElementById('taskInput');
 let buttonAction = document.getElementById('getTask');
 let activeList = document.getElementById('active');
+let doneList = document.getElementById('done');
+let abandonList = document.getElementById('abandon');
+
+function createDeleteButton(listItem) {
+  let deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+
+  deleteButton.addEventListener('click', function() {
+    listItem.remove()
+  });
+  return deleteButton;
+}
+
+function createDoneButton(listItem) {
+  let doneButton = document.createElement('button');
+  doneButton.textContent = "Done"
+
+  //We add a functionality to the done button, which on click will 
+  doneButton.addEventListener('click', function(){
+    listItem.remove(); //remove the element from the active list
+    doneList.append(listItem); //move the element to the done list
+    doneButton.remove();
+    });
+    return doneButton;
+}
+
+function createReactivateButton(listItem){
+  //We create a button for user to move the item to Active list
+  let reactivateButton = document.createElement('button');
+  reactivateButton.textContent = "Reactivate"
+
+  reactivateButton.addEventListener('click', function(){
+    listItem.remove();
+    activeList.append(listItem);
+    reactivateButton.remove();
+
+    let doneButton = createDoneButton(listItem);
+    listItem.appendChild(doneButton);
+  
+    let abandonButton = createAbandonButton(listItem);
+    listItem.appendChild(abandonButton);
+  });
+  return reactivateButton
+}
+
+function createAbandonButton(listItem) {
+  let abandonButton = document.createElement('button');
+  abandonButton.textContent = "Abandon"
+
+  //Same functionality as done button, but it moves element to the abandon list
+  abandonButton.addEventListener('click', function(){
+    listItem.remove();
+    abandonList.append(listItem);
+    abandonButton.remove();
+
+    //We create Reactivate button
+    let reactivateButton = createReactivateButton(listItem)
+    listItem.appendChild(reactivateButton);
+  });
+  return abandonButton
+}
+
 
 buttonAction.addEventListener('click', function() {
-  let newTask = taskInput.value;
-  let listItem = document.createElement('li');
+  let newTask = taskInput.value.trim();
+  if (newTask !== "") {
 
-  listItem.textContent = newTask;
-  activeList.appendChild(listItem)
+    let listItem = document.createElement('li');
+    listItem.textContent = newTask;
 
-  taskInput.value = " "
+    //Create the done button
+    let doneButton = createDoneButton(listItem)
+    listItem.appendChild(doneButton)
+
+    //Create the abandon button
+    let abandonButton = createAbandonButton(listItem)
+    listItem.appendChild(abandonButton)
+
+    //We create the delete button
+    let deleteButton = createDeleteButton(listItem)
+    listItem.appendChild(deleteButton)
+
+    //Adding a new task to the list
+    activeList.appendChild(listItem)
+    taskInput.value = ""
+  };
 });
